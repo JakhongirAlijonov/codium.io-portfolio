a90122f831978e10654a1de2c447709129fbe1a8
jakhongiralijonov <jakhongirweb@gmail.com>
2022-09-26 21:50:32 +0500

    projects section add

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+   });
